b9ef3f5002d00e27ac082ba53f5926e4efc99715
Kimang18 <allenkhun22@gmail.com>
2025-07-29 12:08:43 +0700

    import inject_lora

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .plot import plot_alignment_to_numpy, plot_spectrogram_to_numpy
 from .configuration_vits import VitsConfig
 from .feature_extraction_vits import VitsFeatureExtractor
-from .modeling_vits_training import VitsDiscriminator, VitsModelForPreTraining, slice_segments
+from .modeling_vits_training import VitsDiscriminator, VitsModelForPreTraining, slice_segments, inject_lora_to_conv
 from .romanize import uromanize
 from .khmer_standardize import transform_khmer_sentence
