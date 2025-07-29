0d796544d8170cbaf8fa480e3ca20218298ab6ba
Kimang18 <allenkhun22@gmail.com>
2025-07-29 16:48:24 +0700

    change to hyper-parameter values of original paper

--- run_vits_finetuning.py
+++ run_vits_finetuning.py
@@ -1008,30 +1008,30 @@ def main():
     )
 
     if training_args.do_step_schedule_per_epoch:
-        # gen_lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(
-        #     gen_optimizer, gamma=training_args.lr_decay, last_epoch=-1
+        gen_lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(
+            gen_optimizer, gamma=training_args.lr_decay, last_epoch=-1
+        )
+        disc_lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(
+            disc_optimizer, gamma=training_args.lr_decay, last_epoch=-1
+        )
+        # gen_warmup_sl = torch.optim.lr_scheduler.LinearLR(
+        #     gen_optimizer, start_factor=1e-3, end_factor=1.0, total_iters=num_warmups_steps
         # )
-        # disc_lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(
-        #     disc_optimizer, gamma=training_args.lr_decay, last_epoch=-1
+        # gen_cos_sl = torch.optim.lr_scheduler.CosineAnnealingLR(
+        #     gen_optimizer, T_max=num_training_steps-num_warmups_steps, eta_min=1e-6, last_epoch=-1
         # )
-        gen_warmup_sl = torch.optim.lr_scheduler.LinearLR(
-            gen_optimizer, start_factor=1e-3, end_factor=1.0, total_iters=num_warmups_steps
-        )
-        gen_cos_sl = torch.optim.lr_scheduler.CosineAnnealingLR(
-            gen_optimizer, T_max=num_training_steps-num_warmups_steps, eta_min=1e-6, last_epoch=-1
-        )
-        gen_lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
-            gen_optimizer, schedulers=[gen_warmup_sl, gen_cos_sl], milestones=[num_warmups_steps]
-        )
-        disc_warmup_sl = torch.optim.lr_scheduler.LinearLR(
-            disc_optimizer, start_factor=1e-3, end_factor=1.0, total_iters=num_warmups_steps
-        )
-        disc_cos_sl = torch.optim.lr_scheduler.CosineAnnealingLR(
-            disc_optimizer, T_max=num_training_steps-num_warmups_steps, eta_min=1e-7, last_epoch=-1
-        )
-        disc_lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
-            disc_optimizer, schedulers=[disc_warmup_sl, disc_cos_sl], milestones=[num_warmups_steps]
-        )
+        # gen_lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
+        #     gen_optimizer, schedulers=[gen_warmup_sl, gen_cos_sl], milestones=[num_warmups_steps]
+        # )
+        # disc_warmup_sl = torch.optim.lr_scheduler.LinearLR(
+        #     disc_optimizer, start_factor=1e-3, end_factor=1.0, total_iters=num_warmups_steps
+        # )
+        # disc_cos_sl = torch.optim.lr_scheduler.CosineAnnealingLR(
+        #     disc_optimizer, T_max=num_training_steps-num_warmups_steps, eta_min=1e-7, last_epoch=-1
+        # )
+        # disc_lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
+        #     disc_optimizer, schedulers=[disc_warmup_sl, disc_cos_sl], milestones=[num_warmups_steps]
+        # )
     else:
         gen_lr_scheduler = get_scheduler(
             training_args.lr_scheduler_type,
